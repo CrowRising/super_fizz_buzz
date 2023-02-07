7c36c12c8f1d62b44fe28f227e7a8d9282fc4f4c
Crow Rising <miscbyrd@gmail.com>
2023-02-07 07:15:49 -0700

    Initial commit

--- lib/super_fizz_buzz.rb
+++ lib/super_fizz_buzz.rb
@@ -1,2 +1,29 @@
 # Iterate through the numbers 1 to 1000
 # For each one, print the output according to the rules of Super Fizz Buzz
+
+(1..1000).each do |num|
+  if num % 3 == 0 && num % 5 == 0 && num % 7 == 0
+    p "SuperFizzBuzz"
+
+  elsif num % 7 == 0 && num % 3 == 0
+      p "SuperFizz"
+
+  elsif num % 5 == 0 && num % 7 == 0
+      p "SuperBuzz"
+  
+  elsif num % 3 == 0 && num % 5 == 0
+    p "FizzBuzz"
+
+  elsif num % 3 == 0
+    p "Fizz"
+
+  elsif num % 5 == 0
+    p "Buzz"
+
+  elsif num % 7 == 0
+    p "Super"
+
+  else 
+    p num 
+  end 
+end
